bc7deed3a2310679944e2137ae24bd05afa15043
Jamie Cansdale <jcansdale@gmail.com>
2019-11-04 14:00:37 +0000

    Factor out CommentToBlobUrl

---
+++ GHVS/GitHubUrlUtilities.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using GitHub.Models;
+using GitHub.Primitives;
+using Octokit.GraphQL;
+
+namespace GHVS
+{
+    public class GitHubUrlUtilities
+    {
+        static readonly Regex urlCommentRegex = new Regex($"pull/(?<pull>[0-9]+)([^#]*)#(discussion_r|r)(?<databaseId>[0-9]+)", RegexOptions.Compiled);
+
+        public static async Task<string> CommentToBlobUrl(Func<string, IConnection> connectionFactory, string commentUrl)
+        {
+            var uri = new UriString(commentUrl);
+            var commentInfo = FindCommentInfo(commentUrl);
+            if (commentInfo == default)
+            {
+                return null;
+            }
+
+            var connection = connectionFactory(commentUrl);
+
+            var query = new Query()
+                .Repository(owner: uri.Owner, name: uri.RepositoryName)
+                .PullRequest(number: commentInfo.Pull)
+                .Reviews(first: 100)
+                .Nodes
+                .Select(r => r.Comments(100, null, null, null)
+                .Nodes
+                .Select(c => new
+                {
+                    c.DatabaseId,
+                    c.Path,
+                    c.DiffHunk,
+                    Commit = c.Commit.Oid
+                })
+                .ToList())
+                .Compile();
+
+            var result = await connection.Run(query);
+
+            foreach (var review in result)
+            {
+                foreach (var comment in review.Where(c => c.DatabaseId == commentInfo.DatabaseId))
+                {
+                    foreach (var chunk in DiffUtilities.ParseFragment(comment.DiffHunk))
+                    {
+                        var line = chunk.Lines.Where(l => l.NewLineNumber != -1).LastOrDefault();
+                        var lineNumber = line != null ? $"#L{line.NewLineNumber}" : "";
+                        return $"{uri.ToRepositoryUrl()}/blob/{comment.Commit}/{comment.Path}{lineNumber}";
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        static (int Pull, int DatabaseId) FindCommentInfo(string url)
+        {
+            var uri = new UriString(url);
+
+            var repositoryUrl = uri.ToRepositoryUrl().ToString();
+            var repositoryPrefix = repositoryUrl + "/";
+            if (!url.StartsWith(repositoryPrefix, StringComparison.OrdinalIgnoreCase))
+            {
+                return default;
+            }
+
+            var subpath = url.Substring(repositoryPrefix.Length);
+
+            var match = urlCommentRegex.Match(subpath);
+            if (!match.Success)
+            {
+                return default;
+            }
+
+            var pull = int.Parse(match.Groups["pull"].Value);
+            var databaseId = int.Parse(match.Groups["databaseId"].Value);
+            return (pull, databaseId);
+        }
+    }
+}
--- GHVS/Program.cs
+++ GHVS/Program.cs
@@ -404,8 +404,6 @@ Associated pull requests:");
     [Command(Description = "Open a GitHub URL in Visual Studio")]
     class OpenUrlCommand : GitHubCommandBase
     {
-        static readonly Regex urlCommentRegex = new Regex($"pull/(?<pull>[0-9]+)([^#]*)#(discussion_r|r)(?<databaseId>[0-9]+)", RegexOptions.Compiled);
-
         protected override async Task OnExecute(CommandLineApplication app)
         {
             // Handle x-github-client URIs
@@ -415,7 +413,7 @@ Associated pull requests:");
             url = XGitHubClientUtilities.IgnoreReviewLab(url);
 
             // Convert PR inline comments to blob URLs
-            url = await CommentToBlobUrl(url) ?? url;
+            url = await GitHubUrlUtilities.CommentToBlobUrl(CreateConnection, url) ?? url;
 
             var workingDir = await FindWorkingDirectoryForUrl(url);
             if (workingDir != null)
@@ -463,76 +461,6 @@ Associated pull requests:");
             return null;
         }
 
-        async Task<string> CommentToBlobUrl(string commentUrl)
-        {
-            var uri = new UriString(commentUrl);
-            var commentInfo = FindCommentInfo(commentUrl);
-            if(commentInfo == default)
-            {
-                return null;
-            }
-
-            var connection = CreateConnection(commentUrl);
-
-            var query = new Query()
-                .Repository(owner: uri.Owner, name: uri.RepositoryName)
-                .PullRequest(number: commentInfo.Pull)
-                .Reviews(first: 100)
-                .Nodes
-                .Select(r => r.Comments(100, null, null, null)
-                .Nodes
-                .Select(c => new
-                {
-                    c.DatabaseId,
-                    c.Path,
-                    c.DiffHunk,
-                    Commit = c.Commit.Oid
-                })
-                .ToList())
-                .Compile();
-
-            var result = await connection.Run(query);
-
-            foreach (var review in result)
-            {
-                foreach (var comment in review.Where(c => c.DatabaseId == commentInfo.DatabaseId))
-                {
-                    foreach (var chunk in DiffUtilities.ParseFragment(comment.DiffHunk))
-                    {
-                        var line = chunk.Lines.Where(l => l.NewLineNumber != -1).LastOrDefault();
-                        var lineNumber = line != null ? $"#L{line.NewLineNumber}" : "";
-                        return $"{uri.ToRepositoryUrl()}/blob/{comment.Commit}/{comment.Path}{lineNumber}";
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        static (int Pull, int DatabaseId) FindCommentInfo(string url)
-        {
-            var uri = new UriString(url);
-
-            var repositoryUrl = uri.ToRepositoryUrl().ToString();
-            var repositoryPrefix = repositoryUrl + "/";
-            if (!url.StartsWith(repositoryPrefix, StringComparison.OrdinalIgnoreCase))
-            {
-                return default;
-            }
-
-            var subpath = url.Substring(repositoryPrefix.Length);
-
-            var match = urlCommentRegex.Match(subpath);
-            if (!match.Success)
-            {
-                return default;
-            }
-
-            var pull = int.Parse(match.Groups["pull"].Value);
-            var databaseId = int.Parse(match.Groups["databaseId"].Value);
-            return (pull, databaseId);
-        }
-
         [Argument(0, Description = "The GitHub URL to open")]
         public string Url { get; set; }
     }
@@ -547,7 +475,7 @@ Associated pull requests:");
     {
         protected abstract Task OnExecute(CommandLineApplication app);
 
-        protected Connection CreateConnection(string host = null)
+        protected IConnection CreateConnection(string host = null)
         {
             host = Host ?? host ?? "https://github.com";
 
