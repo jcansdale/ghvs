8ad8430dc0a1308e01743229111a739f9f78ca7d
Jamie Cansdale <jcansdale@github.com>
2019-11-10 21:15:28 +0000

    Add code to find active VSCode folders

--- GHVS/VSCodeUtilities.cs
+++ GHVS/VSCodeUtilities.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace GHVS
 {
@@ -29,6 +32,35 @@ namespace GHVS
             };
 
             return Process.Start(startInfo);
+        }
+
+        public static IEnumerable<string> GetFoldersForPath(string path)
+        {
+            foreach (var folder in GetFolders())
+            {
+                if (File.Exists(path) && path.StartsWith(folder, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    yield return folder;
+                }
+                else if (Directory.Exists(path) && path.Equals(folder, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    yield return folder;
+                }
+            }
+        }
+
+        public static IEnumerable<string> GetFolders()
+        {
+            foreach (var process in Process.GetProcessesByName("CodeHelper"))
+            {
+                var commandLine = ProcessUtilities.GetCommandLine(process.Id);
+                var args = ProcessUtilities.SplitArgs(commandLine);
+
+                if (args.Length == 2)
+                {
+                    yield return args[1];
+                }
+            }
         }
     }
 }
