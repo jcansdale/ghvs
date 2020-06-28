dab7a24943f63ce403e56894d02b102ec2c400e2
Jamie Cansdale <jcansdale@gmail.com>
2020-06-28 01:05:44 +0100

    Add Windows package

---
+++ npx/win-x64/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const { spawn } = require('child_process');
+const path = require('path');
+
+let ghvsFile = path.join(__dirname, "bin/ghvs");
+
+const ghvs = spawn(ghvsFile, process.argv.slice(2));
+
+ghvs.stdout.on('data', (data) => {
+  console.log(data);
+});
+
+ghvs.stderr.on('data', (data) => {
+  console.error(data);
+});
+
+ghvs.on('close', (code) => {
+  process.exit(code)
+});
