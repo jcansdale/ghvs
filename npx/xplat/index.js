165a23f4b8a03cb47e78ced21f381ff7033fd4b5
Jamie Cansdale <jcansdale@gmail.com>
2020-06-28 12:47:22 +0100

    Remove dependancy on npm

--- npx/xplat/index.js
+++ npx/xplat/index.js
@@ -7,10 +7,8 @@ function ghvs(args) {
 
 function npx(args) {
   const npx = require('libnpx')
-  const path = require('path')  
-  const NPM_PATH = path.join(__dirname, '..', 'node_modules', 'npm', 'bin', 'npm-cli.js')  
   const argv = [process.argv[0], process.argv[1]].concat(args);
-  const parsed = npx.parseArgs(argv, NPM_PATH)
+  const parsed = npx.parseArgs(argv)
   npx(parsed)  
 }
 
