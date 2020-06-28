9154133a600e973f28790d4e649398853133f4c9
Jamie Cansdale <jcansdale@gmail.com>
2020-06-28 12:22:40 +0100

    Format data before output

--- npx/linux-x64/index.js
+++ npx/linux-x64/index.js
@@ -8,11 +8,11 @@ let ghvsFile = path.join(__dirname, "bin/ghvs");
 const ghvs = spawn(ghvsFile, process.argv.slice(2));
 
 ghvs.stdout.on('data', (data) => {
-  console.log(data);
+  console.log(`${data}`);
 });
 
 ghvs.stderr.on('data', (data) => {
-  console.error(data);
+  console.error(`${data}`);
 });
 
 ghvs.on('close', (code) => {
