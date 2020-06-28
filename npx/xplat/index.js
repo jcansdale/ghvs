1a4c2276cad0753bbb5fe2919b7cdb09290bed98
Jamie Cansdale <jcansdale@gmail.com>
2020-06-28 12:55:54 +0100

    Add dependancy on npm

--- npx/xplat/index.js
+++ npx/xplat/index.js
@@ -7,8 +7,10 @@ function ghvs(args) {
 
 function npx(args) {
   const npx = require('libnpx')
+  const path = require('path')  
+  const NPM_PATH = path.join(__dirname, 'node_modules', 'npm', 'bin', 'npm-cli.js')
   const argv = [process.argv[0], process.argv[1]].concat(args);
-  const parsed = npx.parseArgs(argv)
+  const parsed = npx.parseArgs(argv, NPM_PATH)
   npx(parsed)  
 }
 
