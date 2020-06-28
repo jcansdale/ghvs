6b8f2978d4943683289e1aebd67815991725b7e1
Jamie Cansdale <jcansdale@gmail.com>
2020-06-28 12:43:27 +0100

    Pass just args to npx

--- npx/xplat/index.js
+++ npx/xplat/index.js
@@ -2,14 +2,14 @@ module.exports = ghvs
 
 function ghvs(args) {
   const package = findPackage()
-  npx(package, args);
+  npx([package].concat(args));
 }
 
-function npx(package, args) {
+function npx(args) {
   const npx = require('libnpx')
   const path = require('path')  
   const NPM_PATH = path.join(__dirname, '..', 'node_modules', 'npm', 'bin', 'npm-cli.js')  
-  const argv = [process.argv[0], process.argv[1], package].concat(args);
+  const argv = [process.argv[0], process.argv[1]].concat(args);
   const parsed = npx.parseArgs(argv, NPM_PATH)
   npx(parsed)  
 }
